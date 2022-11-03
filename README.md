ddf15a2e042c73ee1076f98ab47d3012cee249f2
Wojciech Lawren <wojciech.lawren@gmail.com>
2022-11-03 19:56:23 +0100

    test glen png

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Python TRNG (RDRAND, RDSEED)
 ```
 python setup.py build_ext -i
-python -m pytest  # test_require: (pytest, pytest-benchmark, pypng)
+python -m pytest  # test_require: (pytest, pytest-benchmark, pypng, numpy)
 python setup.py install
 ```
 <img src="/assets/test_rdrand_color.png" style="width: 50vw;" alt="RDRAND IID">
--- tests/test_rdrand_iid.py
+++ tests/test_rdrand_iid.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
-import _rdrand as rd
-
+import numpy as np
 import png  # https://raw.github.com/drj11/pypng/main/code/png.py
+
+import _rdrand as rd
 
 W, H = 1920, 1080
 
-img = [[rd.rand16() for _ in range(W)] for _ in range(H)]
+img = np.array([[rd.rand16() for _ in range(W)] for _ in range(H)])
 
 with open("test_rdrand_grey.png", "wb") as f:
-    w = png.Writer(len(img[0]), len(img), greyscale=True, bitdepth=16)
+    w = png.Writer(img.shape[1], img.shape[0], greyscale=True, bitdepth=16)
     w.write(f, img)
 
-img = [[rd.rand16() for _ in range(W*3)] for _ in range(H)]
+img = np.array([[rd.rand16() for _ in range(W*3)] for _ in range(H)])
 
 with open("test_rdrand_color.png", "wb") as f:
-    w = png.Writer(len(img[0])//3, len(img), greyscale=False, bitdepth=16)
+    w = png.Writer(img.shape[1]//3, img.shape[0], greyscale=False, bitdepth=16)
+    w.write(f, img)
+
+img = np.zeros([H, W*3], int)
+
+for i in range(H):
+    if i % 2:
+        for j in range(W*3):
+            if j % 2:
+                img[i][j] = rd.rand16()
+            continue
+
+
+with open("test_rdrand_glen.png", "wb") as f:
+    w = png.Writer(img.shape[1]//3, img.shape[0], greyscale=False, bitdepth=16)
     w.write(f, img)
